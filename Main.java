827f78d379154692344374687fac9d50528924f2
Mykola0510 <lysenko.mykola@chnu.edu.ua>
2025-12-05 08:03:05 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,45 +1,35 @@
 package ua;
 
 import ua.model.*;
-import ua.model.enums.*;
-import java.time.LocalDate;
+import ua.util.LibraryUtils;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
+        AccessLevel level = AccessLevel.PREMIUM;
+
         Author a1 = new Author("Mark", "Twain", 1835);
-        Reader r1 = new Reader("John", "Smith", "RD001");
+        Reader r1 = new Reader("John", "Smith", "R001", level);
 
-        FileResource file = new FileResource(
-                "http://ebook.com/book.pdf",
-                EBookFormat.PDF,
-                2.5,
-                EBookType.PUBLIC
-        );
+        FileResource file1 = new FileResource("http://book.com/file.pdf", "PDF", 2.5);
+        EBook ebook = new EBook("Tom Sawyer", List.of(a1), "ISBN-11", List.of(file1));
 
-        EBook ebook = EBook.of(
-                "adventures of tom sawyer",
-                List.of(a1),
-                "ISBN-12345",
-                List.of(file)
-        );
+        System.out.println("=== Дані читача ===");
+        System.out.println(r1);
+        System.out.println("Рівень доступу: " + level);
 
-        System.out.println(ebook);
+        System.out.println("Опис доступу: " + LibraryUtils.accessDescription(level));
+        System.out.println("Ліміт книг: " + LibraryUtils.computeLimit(level));
 
-        LibraryAccess la = LibraryAccess.create(
-                r1,
-                ebook,
-                LocalDate.now(),
-                LocalDate.now().plusDays(7)
-        );
-
-        System.out.println(la);
-
-        try {
-            ebook.setTitle(" ");
-        } catch (Exception e) {
-            System.out.println("Validation error: " + e.getMessage());
+        System.out.println("\n=== Демонстрація switch-case ===");
+        switch (level) {
+            case BASIC -> System.out.println("Читач має базовий доступ");
+            case PREMIUM -> System.out.println("Читач має розширений доступ");
+            case VIP -> System.out.println("Читач має VIP доступ");
         }
+
+        System.out.println("\n=== Дані книги ===");
+        System.out.println(ebook);
     }
 }
